6d50ac250d70c69ec5c7eb4b05a3583b927260e3
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-14 15:09:34 -0700

    Added LinkedQueue.cs

---
+++ BinaryRep/LinkedQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryRep
+{
+    class LinkedQueue<T> : QueueInterface<T>
+    {
+        private Node<T> front;
+        private Node<T> rear;
+
+        public LinkedQueue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public T push(T element)
+        {
+            if(element == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (isEmpty())
+            {
+                Node<T> tmp = new Node<T>(element, null);
+                front = tmp;
+                rear = tmp;
+
+            }
+            else
+            {
+                Node<T> tmp = new Node<T>(element, null);
+                rear.next = tmp;
+                rear = tmp;
+            }
+            return element;
+        }
+
+        public T pop()
+        {
+            T tmp;
+            if (isEmpty())
+            {
+                throw new QueueUnderflowException("The queue was empty when pop was invoked");
+
+            }
+            else if(front == rear)
+            {
+                tmp = front.data;
+                front = null;
+                rear = null;
+            }
+            else
+            {
+                tmp = front.data;
+                front = front.next;
+            }
+            return tmp;
+        }
+
+        public Boolean isEmpty()
+        {
+            return ((front == null) && (rear == null));
+        }
+    }
+}
