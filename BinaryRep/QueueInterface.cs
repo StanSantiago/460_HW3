411eed19e938b24a19d374f53ce8163a7461c8e1
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-14 14:47:32 -0700

    Added QueueInterface.cs and QueueUnderflowException.cs

---
+++ BinaryRep/QueueInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryRep
+{
+    interface QueueInterface<T>
+    {
+        T push(T element);
+
+        T pop() /*throw QueueuUnderflowException()*/;
+
+        Boolean isEmpty();
+    }
+}
---
+++ BinaryRep/QueueUnderflowException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryRep
+{
+    class QueueUnderflowException : SystemException
+    {
+        public QueueUnderflowException():base()
+        {}
+
+        public QueueUnderflowException(string message) : base(message)
+        {}
+    }
+}
